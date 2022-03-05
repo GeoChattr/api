581a0f2dd029a237f162e8547d8c83c42b53e6cf
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2022-03-05 10:36:13 -0800

    added socket.io

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,32 @@
-import express from 'express'
-import { Test } from './routes/Test'
-import http from 'http'
+import express from "express";
+import { Test } from "./routes/Test";
+import { Server } from "socket.io";
+import cors from "cors";
+import http from "http";
 
 const app = express();
-const port = 4000
-const name = "GeoChattr"
+const server = http.createServer(app);
+const io = new Server(server);
 
-app.use("/api", Test())
+const port = 4000;
+const name = "GeoChattr";
+
+//Middlewares
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use("/api", Test());
 
 app.get("/", (req, res) => {
-    res.json({ success: true, message: `${name} API` })
-})
+  res.json({ success: true, message: `${name} API` });
+});
 
-app.listen(port, () => {
-    console.log(`Server started on port http://localhost:${port}`)
-})
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+server.listen(port, () => {
+  console.log(`Server started on port http://localhost:${port}`);
+});
