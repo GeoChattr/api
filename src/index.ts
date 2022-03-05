eac1c4112b881e6316b1d9737685cd2bb8eb4afa
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2022-03-05 10:22:04 -0800

    simpified api

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
-import { Server } from "./server/Server";
+import express from 'express'
+import { Test } from './routes/Test'
+import http from 'http'
 
-const server: Server = new Server();
-server.start();
+const app = express();
+const port = 4000
+const name = "GeoChattr"
+
+app.use("/api", Test())
+
+app.get("/", (req, res) => {
+    res.json({ success: true, message: `${name} API` })
+})
+
+app.listen(port, () => {
+    console.log(`Server started on port http://localhost:${port}`)
+})
