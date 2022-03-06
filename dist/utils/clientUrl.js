9c2c676deb833f3fe20810c6884da59cb32f5128
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2022-03-05 17:32:38 -0800

    re-added dist

---
+++ dist/utils/clientUrl.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.clientUrl = void 0;
+exports.clientUrl = "http://localhost:3000";
+//# sourceMappingURL=clientUrl.js.map
