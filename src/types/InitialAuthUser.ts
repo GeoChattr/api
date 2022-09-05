7d914ed77ca88702d4df32eac9535b6e38843d7f
Shubham Patil <shubhampatilsd@gmail.com>
2022-09-05 11:30:12 -0700

    railway stop annoying me

--- src/types/InitialAuthUser.ts
+++ src/types/InitialAuthUser.ts
@@ -1,13 +1,13 @@
-import { AuthProvider } from "@prisma/client";
+// import { AuthProvider } from "@prisma/client";
 
-export interface InitialAuthUser {
-  id: string;
-  email?: string;
-  profilePicture: string;
-  displayName: string;
-  username: string;
-  location: string;
-  provider?: AuthProvider;
-  createdAt?: Date;
-  updatedAt?: Date
-}
+// export interface InitialAuthUser {
+//   id: string;
+//   email?: string;
+//   profilePicture: string;
+//   displayName: string;
+//   username: string;
+//   location: string;
+//   provider?: AuthProvider;
+//   createdAt?: Date;
+//   updatedAt?: Date
+// }
